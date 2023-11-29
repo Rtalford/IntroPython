18cb87f428230cd7b37e70cbfd0fcd686170d58d
Reynelle <RTalford13@gmail.com>
2023-11-29 05:24:15 -0500

    updates

---
+++ Project1/projectClassNotes.py
@@ -0,0 +1,15 @@
+#intialization 
+
+#
+
+while True: 
+  # get the inputs
+  username = input('What is your first name? ')
+  print(username)
+  break
+
+  # do your username testing 
+
+  #do your password testing
+
+  pass
