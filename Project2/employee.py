7eaa5f714902a67003ad70e71a1d882c5b6b70e0
Reynelle <RTalford13@gmail.com>
2023-12-19 05:01:08 -0500

    added code

--- Project2/employee.py
+++ Project2/employee.py
@@ -3,7 +3,14 @@ from datetime import datetime
 class Employee: 
 
   #constructor with __init__
-  
-  def __init__ (self, name = "Florence", job_title = "Software Engineer", department = "Python developer", salary = 195,000 , hire_year = 2023): 
 
-my_employee 
+  def __init__(self, name = "", job_title = "", department = "", salary = 0, hire_year = 0): 
+    self.name = name 
+    self.job_title = job_title
+    self.department = department
+    self.salary = salary
+    self.hire_year = hire_year
+
+
+  
+  my_first_employee = ("Florence", "Software Engineer", "Python Developer", 195,000, 2023)
