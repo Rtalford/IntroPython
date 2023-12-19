96e8ef47d4a77e11752d921346a5c0416c5df889
Reynelle <RTalford13@gmail.com>
2023-12-19 05:11:35 -0500

    revised code

--- Project2/employee.py
+++ Project2/employee.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+# from datetime import datetime
 
 class Employee: 
 
@@ -12,5 +12,10 @@ class Employee:
     self.hire_year = hire_year
 
 
-  
-  my_first_employee = ("Florence", "Software Engineer", "Python Developer", 195,000, 2023)
+  #class instance or object created 
+    
+  first_employee = ("Florence", "Software Engineer", "Python Developer", 195000, 2023)
+  second_employee = ("Thelma", "Chemist", "Food & Drug Adminstration", 585000, 1990)
+
+  print(first_employee)
+  print(second_employee)
