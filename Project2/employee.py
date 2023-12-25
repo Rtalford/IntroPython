a533d3a6f7667ff29a8592f65717000223f711fb
Reynelle <RTalford13@gmail.com>
2023-12-25 13:57:08 -0500

    updates to project2 notes

--- Project2/employee.py
+++ Project2/employee.py
@@ -22,15 +22,9 @@ class Employee:
     #return Employee(current_year - self.hire_year)
   
       #module used to find current_year/ then use to figure out ___years_worked__
-      #def __It__(self, other):
-      #selfdate = datetime(self.year, self.month, self.day)
-      #otherdate = datetime(other.year, self.month, self.day)
-      #if selfdate < otherdate:
-          #return True 
-      #return False
   
-      #def __It__(self, other): 
-      #current_year = datetime(self.)
+      #def __years_worked__(self):
+        #today = datetime.datetime.now()
     
   #def__total_expense__()
     
