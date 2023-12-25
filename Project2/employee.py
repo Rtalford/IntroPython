c2e3f8b4d05be6c232e73b137f22d2b07f5803d2
Reynelle <RTalford13@gmail.com>
2023-12-25 11:38:33 -0500

    updates

--- Project2/employee.py
+++ Project2/employee.py
@@ -11,10 +11,10 @@ class Employee:
     self.salary = salary
     self.hire_year = hire_year
 
-  #def__str__(self):
-    #return f'({self.name}, {self.job_title}, {self.department}, {self.salary}, 
-    #{self.hire_year})'
-
+  def__str__(self):
+    return f'({self.name}, {self.job_title}, {self.department}, {self.salary}, 
+    {self.hire_year})'
+   
   #? Do we need to use the other build-in class methods; __eq__, __add__,
 
   #def__years_worked__(self, other):   #__sub__ ?
@@ -25,38 +25,38 @@ class Employee:
 
   #Accessor method : returns an attribute to the user/ access attributes 
     
-  #def get_name(self): 
-    #return self.name
+  def get_name(self): 
+    return self.name
 
-  #def get_job_title(self): 
-    #return self.job_title
+  def get_job_title(self): 
+    return self.job_title
 
-  #def get_department(self): 
-    #return self.department 
+  def get_department(self): 
+    return self.department 
 
-  #def get_salary(self): 
-    #return self.salary
+  def get_salary(self): 
+    return self.salary
 
-  #def get_hire_year(self): 
-    #return self.hire_year 
+  def get_hire_year(self): 
+    return self.hire_year 
   
     
   #Mutator method : lets the user change the attribute
     
-  #def set_name(self, new_name):
-    #self.name = new_name
+  def set_name(self, new_name):
+    self.name = new_name
 
-  #def set_job_title(self, new_job_title):
-    #self.job_title = new_job_title
+  def set_job_title(self, new_job_title):
+    self.job_title = new_job_title
 
-  #def set_department(self, new_department):
-    #self.department = new_department
+  def set_department(self, new_department):
+    self.department = new_department
 
-  #def set_salary(self, new_salary):
-    #self.salary = new_salary
+  def set_salary(self, new_salary):
+    self.salary = new_salary
 
-  #def set_hire_year(self, new_hire_year):
-    #self.hire_year = new_hire_year
+  def set_hire_year(self, new_hire_year):
+    self.hire_year = new_hire_year
     
 
   #class instance or creating objects / class objects
